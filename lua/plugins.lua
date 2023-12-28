ee5e4bb383d5645f2a83f5b530adcb8a9d7f3ec5
Marc Mauger <simianarmy@gmail.com>
2023-12-28 16:27:01 -0500

    added tmux navigator plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -26,5 +26,22 @@ return {
   {'L3MON4D3/LuaSnip'},
   {'rafamadriz/friendly-snippets'},
   "github/copilot.vim",
-  "laytan/cloak.nvim"
+  "laytan/cloak.nvim",
+  {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
 }
